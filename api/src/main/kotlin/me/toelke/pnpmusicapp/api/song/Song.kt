180b326ea158d892b09cbcb4d445079bac24c17a
implex1v <implex1v@gmail.com>
2022-09-16 18:31:25 +0200

    (api) Add indices for song

--- api/src/main/kotlin/me/toelke/pnpmusicapp/api/song/Song.kt
+++ api/src/main/kotlin/me/toelke/pnpmusicapp/api/song/Song.kt
@@ -2,13 +2,16 @@ package me.toelke.pnpmusicapp.api.song
 
 import me.toelke.pnpmusicapp.api.uuid
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
 data class Song(
     @Id
     val id: String = uuid(),
+    @Indexed
     val name: String,
     val location: String,
+    @Indexed
     val tags: List<String> = emptyList(),
 )
