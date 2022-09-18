6e7b4d39f2b6cba4cee1636afdeb0c92924844f0
implex1v <implex1v@gmail.com>
2022-09-18 10:42:10 +0200

    (ui) Display song details

--- ui/src/lib/Models.ts
+++ ui/src/lib/Models.ts
@@ -1,5 +1,13 @@
 export type Song = {
     id: string,
     name: string,
+    detail: SongDetail | undefined,
     tags: Array<string>,
+}
+
+export type SongDetail = {
+    title: string,
+    album: string | undefined,
+    artist: string,
+    seconds: number,
 }
