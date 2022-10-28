9eeabd1c60fffc337d71301165311a3411a0866e
implex1v <implex1v@gmail.com>
2022-10-28 21:31:46 +0200

    (ui) Evaluate API host from env directly

--- ui/next.config.js
+++ ui/next.config.js
@@ -3,7 +3,4 @@ module.exports = {
   output: "standalone",
   reactStrictMode: true,
   swcMinify: true,
-  serverRuntimeConfig: {
-    apiHost: process.env.APP_API_HOST ?? "http://localhost:8080"
-  }
 }
--- ui/pages/api/config.ts
+++ ui/pages/api/config.ts
@@ -1,8 +1,7 @@
 import getConfig from "next/config";
 
 export default function config(req, res) {
-    const { serverRuntimeConfig } = getConfig()
     res
         .status(200)
-        .json({ api: serverRuntimeConfig.apiHost })
+        .json({ api: process.env.APP_API_HOST ?? "http://localhost:8080" })
 }
